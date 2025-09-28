6e48b5c02034b4db28a0678d0956760dc518fa09
OneKnown <104769113+OneKnow654@users.noreply.github.com>
2025-09-28 22:58:12 +0530

    Add files via upload

---
+++ models/ContactEmail.js
@@ -0,0 +1,25 @@
+// models/ContactEmail.js
+const mongoose = require('mongoose');
+
+const contactEmailSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  submittedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('ContactEmail', contactEmailSchema);
